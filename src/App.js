0d8110ce2d8f60dc2ed7bf8367e52af5108f205b
prerna-p <prernasocialid@gmail.com>
2020-06-27 18:46:40 -0400

    router added to project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import './App.css';
+import { HashRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer';
 import Landing from './components/Landing';
 import store from './store';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Provider store={store}>
-        <Navbar />
-        <Landing />
-        <Footer />
-      </Provider>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Provider store={store}>
+          <Router>
+            <div>
+              <Navbar />
+              <Landing />
+              <Footer />
+            </div>
+          </Router>
+        </Provider>
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
+/**
+ * HashRouter helps for deployment to GitHub pages
+ * vs BrowserRouter
+ */
