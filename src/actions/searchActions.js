a2e62844155a9ca57b35022712a0198f151bc59f
prerna-p <prernasocialid@gmail.com>
2020-07-03 21:37:32 -0400

    added fetchMovie action

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -1,10 +1,14 @@
-import { SEARCH_MOVIE, FETCH_MOVIES } from './types';
+import {
+  SEARCH_MOVIE,
+  FETCH_MOVIES,
+  FETCH_MOVIE
+} from './types';
 import axios from 'axios';
-
+import { APIKey } from '../APIKey'
 const API_KEY = `${process.env.REACT_APP_API_KEY}`;
-const URL = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+const URL = `http://www.omdbapi.com/?apikey=${APIKey}`;
 
-export const searchMovie = (text) => dispatch => {
+export const searchMovie = text => dispatch => {
   dispatch({
     type: SEARCH_MOVIE,
     payload: text,
@@ -12,9 +16,20 @@ export const searchMovie = (text) => dispatch => {
 }
 
 export const fetchMovies = text => dispatch => {
-  axios.get(URL + `&s=${text}`)
+  axios
+    .get(URL + `&s=${text}`)
     .then(response => dispatch({
       type: FETCH_MOVIES,
+      payload: response.data.Search,
+    }))
+    .catch(err => console.log(err));
+}
+
+export const fetchMovie = id => dispatch => {
+  axios
+    .get(URL + `&i=${id}`)
+    .then(response => dispatch({
+      type: FETCH_MOVIE,
       payload: response.data,
     }))
     .catch(err => console.log(err));
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import SearchForm from './SearchForm';
+import Spinner from './Spinner';
+import MoviesContainer from './MoviesContainer'
 
 export class Landing extends Component {
   render() {
+    const { loading } = this.props;
+
     return (
       <div className="container">
         <SearchForm />
+        {loading ? <Spinner /> : <MoviesContainer />}
       </div>
     )
   }
 }
 
-export default Landing
+const mapStateToProps = state => ({
+  loading: state.movies.loading,
+})
+
+export default connect(mapStateToProps)(Landing);
